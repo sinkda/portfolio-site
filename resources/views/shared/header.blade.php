0d418ba1f98214aa034929a5176c6bc1c51fb536
Sinkda <sinkda@gmail.com>
2022-04-17 09:44:13 -0400

    Add close mobile nav on click away

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -6,7 +6,7 @@
         </a>
     </div>
 
-    <div class="flex md:hidden items-center" x-on:click="mobileNavOpen = !mobileNavOpen">
+    <div class="flex md:hidden items-center" x-on:click="mobileNavOpen = !mobileNavOpen" x-on:click.away="mobileNavOpen = false">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
         </svg>
